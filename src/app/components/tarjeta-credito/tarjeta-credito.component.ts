aebea46bc39a9aed22135e132d0058df3f0b87e9
Oscar Rojas <oscar.rojas@akros.tech>
2022-03-04 21:19:46 -0600

    commit

---
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-tarjeta-credito',
+  templateUrl: './tarjeta-credito.component.html',
+  styleUrls: ['./tarjeta-credito.component.css']
+})
+export class TarjetaCreditoComponent implements OnInit {
+
+  listTarjetas: any[] = [
+    { titulo: 'Juan Perez', numeroTarjeta: '2334245356', fechaExipra: '11/23', cvv: '123' },
+    { titulo: 'Miguel Gonzalez', numeroTarjeta: '34342342342', fechaExipra: '12/24', cvv: '312' }
+  ];
+
+
+  form: FormGroup;
+
+  constructor( private fb: FormBuilder, private toastr: ToastrService ) { 
+    this.form = this.fb.group({ 
+      titulo: ['', Validators.required],
+      numeroTarjeta: ['',[Validators.required,Validators.maxLength(16),Validators.minLength(16)]],
+      fechaExipra: ['',[Validators.required,Validators.maxLength(5),Validators.minLength(5)]],
+      cvv: ['',[Validators.required,Validators.maxLength(3),Validators.minLength(3)]]
+     })
+   }
+
+  ngOnInit(): void {
+  }
+
+  agregarTarjeta() { 
+    
+
+    const tarjeta: any = { 
+      titulo: this.form.get('titulo')?.value,
+      numeroTarjeta: this.form.get('numeroTarjeta')?.value,
+      fechaExipra: this.form.get('fechaExipra')?.value,
+      cvv: this.form.get('cvv')?.value
+     }
+
+     this.listTarjetas.push(tarjeta);
+     this.toastr.success('La tarjeta fue registrada con exito', 'Tarjeta Registrada');
+     this.form.reset();
+   }
+
+   eliminarTarjeta(index: number) { 
+     this.listTarjetas.splice(index, 1);
+     this.toastr.success('La tarjeta fue eliminada con exito', 'Tarjeta Eliminada');
+    }
+
+}
